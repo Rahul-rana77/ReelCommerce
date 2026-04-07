f599b3b21931b6edf2dcabb2817cafae3f7fddb6
Rahul Rana <rahulranawork11@gmail.com>
2026-04-07 14:59:02 +0530

    auth middleware fix

--- server/src/middlewares/auth.middleware.js
+++ server/src/middlewares/auth.middleware.js
@@ -1,23 +1,40 @@
 import adminModel from '../models/admin.model.js';
+import userModel from '../models/user.model.js';
 import jwt from 'jsonwebtoken';
 
-const authenticateAdminMiddleware = async (req, res, next) => {
+const authenticateMiddleware = async (req, res, next) => {
     try {
-        const token = req.cookies.token;
-        if (!token) {
-            return res.status(401).json({ message: 'Please login first' });
+        if(adminToken) {
+            const adminToken = req.cookies.adminToken;
+            if (!adminToken) {
+                return res.status(401).json({ message: 'Please login first' });
+            }
+            const decoded = jwt.verify(adminToken, process.env.JWT_SECRET);
+            const admin = await adminModel.findById(decoded.id);
+            if (!admin) {
+                return res.status(401).json({ message: 'Admin not found' });
+            }
+            req.admin = admin;
+            next();
         }
-        const decoded = jwt.verify(token, process.env.JWT_SECRET);
-        const admin = await adminModel.findById(decoded.id);
-        if (!admin) {
-            return res.status(401).json({ message: 'Admin not found' });
+        if(token){
+            const token = req.cookies.token;
+            if (!token) {
+                return res.status(401).json({ message: 'Please login first' });
+            }
+            const decoded = jwt.verify(token, process.env.JWT_SECRET);
+            const user = await userModel.findById(decoded.id);
+            if (!user) {
+                return res.status(401).json({ message: 'User not found' });
+            }
+            req.user = user;
+            next();
         }
-        req.admin = admin;
-        next();
     } catch (error) {
-        return res.status(401).json({ message: 'Invalid token' });
+        console.error('Authentication error:', error);
+        res.status(401).json({ message: error.message });
     }
 };
 
 
-export default authenticateAdminMiddleware;
+export default authenticateMiddleware;
